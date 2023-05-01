c5edd8ea84d068a466ab40a7bcb57c276e4bcf7b
Michael Sipayung <Michaelsipayung123@gmail.com>
2023-05-01 12:25:13 +0700

    fix std::bind

--- source/bind.h
+++ source/bind.h
@@ -7,7 +7,16 @@
 #include <iostream>
 #include <vector>
 #include <functional> //header for std::bind
-bool check_size(const std::string &s, std::string::size_type sz){
+#include <algorithm>
+bool checking_size(const std::string &s, std::string::size_type sz){
     return s.size()>=sz;
+}
+//using std::bind as function adaptor
+//modify behavior of lamda
+void modLambda(const std::vector<std::string>&word){
+    //generate a callable object using std::bind 
+    auto wc  = std::find_if(word.begin(),word.end(),std::bind(checking_size,std::placeholders::_1,6));
+    std::cout<<"modify lambda version using std::bind"<<std::endl;
+    std::cout<<"result : "<< *wc<<std::endl;
 }
 #endif
