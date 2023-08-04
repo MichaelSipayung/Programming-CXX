5b15e8c3bf36e7827c15a889080b23cea69b2221
Michael Sipayung <Michaelsipayung123@gmail.com>
2023-08-04 07:44:36 +0700

    insertion and searching on linked list fix

--- source/problem.cpp
+++ source/problem.cpp
@@ -7,7 +7,8 @@ using std::string;
 using std::vector;
 #include <algorithm>
 #include <cmath>
-namespace Problem {
+#include <set>
+namespace problem {
 // result must store on global scope, since we want to use it on
 // showResult function
 vector<int> result;
@@ -20,7 +21,6 @@ void combinationUtil(int arr[], int data[], int start, int end, int index,
     for (int j = 0; j < r; j++) {
       result.push_back(data[j]);
     }
-    // printf("\n");
     return;
   }
   // replace index with all possible elements. The condition
@@ -33,7 +33,7 @@ void combinationUtil(int arr[], int data[], int start, int end, int index,
   }
 }
 // show combination result, return void
-void showResult() {
+void show_result() {
   for (const auto &item : result)
     cout << item << " | ";
 }
@@ -42,14 +42,13 @@ vector<vector<int>> threeSum(const vector<int> &nums) {
   int arr[3];
   std::copy(nums.begin(), nums.end(), arr + 0);
   int data[3];
-  combinationUtil(arr, data, 0, (int)nums.size() - 1, 0, 3);
-  long int total_com =
-      fact((int)nums.size()) / (6 * (fact((int)nums.size() - 3)));
+  combinationUtil(arr, data, 0, static_cast<int>(nums.size()) - 1, 0, 3);
+  const long int total_com = fact(static_cast<int>(nums.size())) /
+                             (6 * (fact(static_cast<int>(nums.size()) - 3)));
   int temp_obj[4][3];
   vector<vector<int>> temp;
-  int total;
   for (int i = 0; i < total_com; ++i) {
-    total = 0;
+    int total = 0;
     for (int j = 0; j < 3; ++j) {
       temp_obj[i][j] = result[i * 3 + j];
       total += temp_obj[i][j];
@@ -75,7 +74,7 @@ ListNode *addTwoNumbers_r(ListNode *link1, ListNode *link2, int carry) {
   if (!link1 && !link2) {
     // there is still carry result or remaining number
     if (carry) {
-      auto temp = new ListNode(carry); // example 999 + 99 = 8901
+	    const auto temp = new ListNode(carry); // example 999 + 99 = 8901
       return temp; // return carry in the right most place, carry is one
     }
     // stop the recursive operation, since carry is 0, link1 and link2
@@ -116,7 +115,7 @@ ListNode *addTwoNumbers_r(ListNode *link1, ListNode *link2, int carry) {
 }
 // adding two singly linked list, non-recursive version
 // stopping condition : l1 and l2 and carry is null
-ListNode *addTwoNumbers_m(ListNode *list1, ListNode *list2) {
+ListNode *add_two_numbers_m(ListNode *list1, ListNode *list2) {
   // adding dummy node
   auto *dummyHead = new ListNode(0);
   // remember the current node
@@ -127,9 +126,9 @@ ListNode *addTwoNumbers_m(ListNode *list1, ListNode *list2) {
   // condition to satisfy
   // list1 or list2 not null and carry is still not 0
   while (list1 != nullptr || list2 != nullptr || carry != 0) {
-    int x = list1 ? list1->val : 0; // if there is existed to add
-    int y = list2 ? list2->val : 0; // same as condition list1
-    int sum = carry + x + y;        // store the total number
+	  const int x = list1 ? list1->val : 0; // if there is existed to add
+	  const int y = list2 ? list2->val : 0; // same as condition list1
+	  const int sum = carry + x + y;        // store the total number
     carry = sum / 10;               // only contain o or 1 as a value
     // store the solution on curr->next node
     // if sum less than ten, store sum to the
@@ -147,7 +146,7 @@ ListNode *addTwoNumbers_m(ListNode *list1, ListNode *list2) {
 // demonstration of adding two number from singly linked list A and B
 // number are store in reverse order
 // the result also in reverse order
-void testAddTwoNumber() {
+void test_add_two_number() {
   // store number reverse order
   ListNode left_one(9);
   ListNode left_nextOne(9, &left_one);
@@ -156,11 +155,11 @@ void testAddTwoNumber() {
   ListNode right_one(9);
   ListNode right_nextOne(9, &right_one);
   ListNode right_end(9, &right_nextOne);
-  auto resultLinked = addTwoNumbers_r(&left_end, &right_end);
-  traverseLikedList(resultLinked);
+  const auto resultLinked = addTwoNumbers_r(&left_end, &right_end);
+  traverse_liked_list(resultLinked);
 }
 // show all number one by one from singly linked list
-void traverseLikedList(ListNode *ls) {
+void traverse_liked_list(ListNode *ls) {
   cout << " result [";
   while (ls) { // where there is a number
     cout << ls->val << " , ";
@@ -169,30 +168,32 @@ void traverseLikedList(ListNode *ls) {
   cout << " ]" << endl;
 }
 // recursive fast exponential function with time complexity O(log n)
-long fastExp(const int &_base, const int &_power) {
+long fast_exp(const int &_base, const int &_power) {
   if (_power == 0)
     return 1;
-  auto x = fastExp(_base, _power / 2);
-  //recursively doing exponentiation
-  return _power % 2 == 0 ? (long)std::pow(x, 2) : _base * (long)std::pow(x, 2);
+  auto x = fast_exp(_base, _power / 2);
+  // recursively doing exponentiation
+  return _power % 2 == 0 ? static_cast<long>(std::pow(x, 2)) : _base * static_cast<long>(std::pow(x, 2));
 }
 // remove k digits from string and find the smallest substring
 //_x is a string that contain non-negative number and k is number
 // of digit we will remove from _x, return the smallest number0
-std::string removeKDigits(const string &_x, int &_k) {
+std::string remove_k_digits(const string &_x, int &_k) {
   // special case, if there are _k digits to remove
   //  and _k==len(_x) and k=0, and nothing to do
   if (_x.empty() || _x.size() == _k || _k < 0)
     return {"0"};
   vector<char> stack; // store the object
-  for (const auto &item : _x) {
-    // remove element, removing must meet the condition
-    // where the k times is limit for removing a number from stack
-    while (_k > 0 && !stack.empty() && stack.back() > item) {
-      stack.pop_back();
-      --_k;
-    }
-    stack.push_back(item);
+  for (const auto &item : _x)
+  {
+	  // remove element, removing must meet the condition
+	  // where the k times is limit for removing a number from stack
+	  while (_k > 0 && !stack.empty() && stack.back() > item)
+	  {
+		  stack.pop_back();
+		  --_k;
+	  }
+	  stack.push_back(item);
   }
   // while there is an element to pop, until the k times
   // meet, the top conditional statement that not guarantee
@@ -205,7 +206,7 @@ std::string removeKDigits(const string &_x, int &_k) {
   // store the minimum number, if there is a leading zero, skip it
   // using conditional statement to make sure leading zero is passed
   string store;
-  auto find_zero = std::find(std::begin(stack), std::end(stack), '0');
+  const auto find_zero = std::find(std::begin(stack), std::end(stack), '0');
   if (find_zero != std::begin(stack))
     for (const auto &item : stack)
       store += item;
@@ -226,51 +227,87 @@ std::string removeKDigits(const string &_x, int &_k) {
   return store;
 }
 // definition of searchList, for searching an item on linked list
-linkedList *searchList(linkedList *_ls, const string &_data) {
-  if (_ls == nullptr)
+linkedList *search_list(linkedList *ls, const string &data) {
+  if (ls == nullptr) //if ls is nullptr, return nullptr
     return nullptr;
-  return _ls->item == _data ? _ls : searchList(_ls->next, _data);
+  return ls->item == data ? ls : search_list(ls->next, data);
 }
 // insert an element on singly linked list
 // update head of data structure using temporary variable
-void insert_list(linkedList **_ls, const string &_item) {
-  linkedList *temp = nullptr; // temporary variable
-  temp = new linkedList;      // allocate new object
-  temp->item = _item;         // assign item to temporary object
-  temp->next = *_ls;          // temporary.next point to *ls, remember the head
-  *_ls = temp;                // copy temp to the place pointed by *ls
+void insert_list(linkedList **ls, const string &item)
+{
+	linkedList* temp = nullptr; // temporary variable
+	temp = new linkedList(); // allocate new object
+	temp->item = item; // assign item to temporary object
+  //tem->item = thomas, ls point to jack
+  //example : current linked-list : knuth -> jack -> miller
+	temp->next = *ls; // temporary.next point to *ls, remember the head
+  //temp->next = jack ->miller
+	*ls = temp; // copy temp to the place pointed by *ls
+  //last linked-list : knuth -> thomas ->jack -> miller
 }
+
 // find the predecessor because it points to the doomed node
 // so its next pointer must be changed, predecessor is element
 // before linked-list _rh on _lh, look successor for further explanation
-linkedList *item_head(linkedList *_lh, linkedList *_rh) {
-  // check if _ls point to something
-  if ((_lh == nullptr) || (_lh->next == nullptr))
-    return nullptr;
-  // traverse the next linked list
-  // if it's, return head otherwise look recursively to next
-  return _lh->next == _rh ? _lh : item_head(_lh->next, _rh);
+linkedList *item_head(linkedList *lh, linkedList *rh)
+{
+	// check if _ls point to something
+	if ((lh == nullptr) || (lh->next == nullptr))
+		return nullptr;
+	// traverse the next linked list
+	// if it's, return head otherwise look recursively to next
+	return lh->next == rh ? lh : item_head(lh->next, rh);
 }
 // delete linked-list from given position _rh from _lh
-void delete_list(linkedList **_lh, linkedList **_rh) {
-  linkedList *p = nullptr;           // item pointer
-  linkedList *predecessor = nullptr; // predecessor pointer
-  p = *_lh;
+void delete_list(linkedList **lh, linkedList **rh) {
+	linkedList *predecessor = nullptr; // predecessor pointer
+	const linkedList* p = *lh;
   // find the predecessor of _rh
-  predecessor = item_head(*_lh, *_rh);
+  predecessor = item_head(*lh, *rh);
   // if predecessor is null, take all _lh.next to head or move all next element
   // otherwise _lh.next contain the linked-list _th next
-  predecessor == nullptr ? *_lh = p->next : predecessor->next = (*_rh)->next;
-  free(*_rh); // free memory used by node
+  predecessor == nullptr ? *lh = p->next : predecessor->next = (*rh)->next;
+  free(*rh); // free memory used by node
 }
 // auxiliary function to tell the successor of the linked-list
 //_ls is the linked-list which consist of some pointer to it
 //_rh is the given list we want to know its successor
-linkedList *successor(linkedList *_lh, linkedList *_rh) {
-  if ((_lh == nullptr) ||
-      (_lh->next == nullptr)) // if given linked-list point to nothing
+linkedList *successor(const linkedList *lh, linkedList *rh) {
+  if ((lh == nullptr) ||
+      (lh->next == nullptr)) // if given linked-list point to nothing
     return nullptr;           // point to nothing return null pointer
   // recursively look for the successor
-  return (_lh == _rh ? _lh->next : successor(_lh->next, _rh));
+  return (lh == rh ? lh->next : successor(lh->next, rh));
+}
+// pangram problem : A pangram is a string that contains every letter of the
+// alphabet. Given a sentence determine whether it is a pangram in the English
+// alphabet. Ignore case. Return either pangram or not pangram as appropriate.
+string pangram(const string &s)
+{
+	std::set<char> temp;
+	if (s.size() < 26)
+		return {"not pangram"};
+	if (!s.empty() && s.size() <= static_cast<unsigned>(1e3))
+		for (const char i : s)
+		{
+			if (isalpha(i) || isspace(i))
+			{
+				if (isspace(i))
+					continue;
+				temp.insert(static_cast<char>(tolower(i)));
+			}
+			else
+				return {"not pangram"};
+		}
+	else
+		return {"not pangram"};
+	return (temp.size() == 26 ? "pangram" : "not pangram");
+}
+//power sum definition
+int power_sum(const int x, const int n )
+{
+  const auto max= static_cast<int>(std::pow(x, (1.0 / n)));
+  return  max;
 }
 } // namespace Problem
--- source/problem.h
+++ source/problem.h
@@ -9,28 +9,29 @@ using std::vector;
 #include <algorithm>
 #include <stack>
 #include <string>
-namespace Problem {
-typedef struct linkedList linkedList;
-typedef struct ListNode ListNode;
+namespace problem {
+typedef struct linked_list linkedList;
+typedef struct list_node ListNode;
 // definition for singly-linked list
-struct ListNode {
+struct list_node {
   int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  explicit ListNode(int x) : val{x}, next{nullptr} {}
-  ListNode(int x, ListNode *next) : val{x}, next{next} {}
-  bool operator==(const ListNode &other) const {
+  list_node *next;
+  list_node() : val(0), next(nullptr) {}
+  explicit list_node(const int x) : val{x}, next{nullptr} {}
+  list_node(const int x, list_node *next) : val{x}, next{next} {}
+  bool operator==(const list_node &other) const {
     return val == other.val && next == other.next;
   }
 };
 // single linked list declaration
-struct linkedList {
+struct linked_list {
   string item;
-  linkedList *next;
-  linkedList(const string &item, linkedList *next) : item(item), next(next) {}
-  linkedList() : item(string(" ")), next(nullptr) {}
-  explicit linkedList(const string &_s) : item(_s), next(nullptr) {}
-  bool operator==(const linkedList &other) const {
+  linked_list *next;
+  //item pass by value and calling std::move for every ctor
+  linked_list(string item, linked_list *next) : item(std::move(item)), next(next) {}
+  linked_list() : item(string(" ")), next(nullptr) {}
+  explicit linked_list(string s) : item(std::move(s)), next(nullptr) {}
+  bool operator==(const linked_list &other) const {
     return item == other.item && next == other.next;
   }
 };
@@ -47,10 +48,9 @@ void binarySearch(vector<Object> &_x, const Object &_data) {
   std::sort(_x.begin(), _x.end());
   size_t begin = 0;
   size_t end = _x.size();
-  size_t middle;
   while (begin <= end) // accept the same range but not the outside
   {
-    middle = begin + (end - begin) / 2; // begin + (half)
+    size_t middle = begin + (end - begin) / 2; // begin + (half)
     if (_data < _x[middle]) {
       end = middle - 1; // move left [begin, middle-1]
     } else if (_data > _x[middle]) {
@@ -64,19 +64,21 @@ void binarySearch(vector<Object> &_x, const Object &_data) {
 }
 void combinationUtil(int arr[], int data[], int start, int end, int index,
                      int r);
-void showResult();
+void show_result();
 long int fact(long int);
-ListNode *addTwoNumbers_r(ListNode *, ListNode *, int carry = 0);
-ListNode *addTwoNumbers_m(ListNode *, ListNode *);
-void testAddTwoNumber();
-void traverseLikedList(ListNode *);
-long fastExp(const int &, const int &);
-std::string removeKDigits(const string &, int &);
+auto addTwoNumbers_r(ListNode*, ListNode*, int carry = 0) -> ListNode*;
+ListNode *add_two_numbers_m(ListNode *, ListNode *);
+void test_add_two_number();
+void traverse_liked_list(ListNode *);
+long fast_exp(const int &, const int &);
+std::string remove_k_digits(const string &, int &);
 // searching on linked list
-linkedList *searchList(linkedList *, const string &);
+linkedList *search_list(linkedList *, const string &);
 void insert_list(linkedList **, const string &);
 linkedList *item_head(linkedList *, linkedList *);
 void delete_list(linkedList **, linkedList **);
-linkedList *successor(linkedList *, linkedList *);
+linkedList *successor(const linkedList *, linkedList *);
+string pangram(const string&);
+int power_sum(int, int);
 } // namespace Problem
 #endif
